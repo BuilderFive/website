78b69586b8e0d63f03b49fe8992a9d31a7a242e6
Wrys <wrysplays@gmail.com>
2024-03-12 10:36:58 -0400

    CSS File Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        "xxs": '10px'
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
@@ -13,7 +16,25 @@ module.exports = {
           background: "hsl(var(--btn-background))",
           "background-hover": "hsl(var(--btn-background-hover))",
         },
+        primary: {
+          100: "hsl(var(--primary-background-1))",
+          200: "hsl(var(--primary-background-2))",
+          300: "hsl(var(--primary-background-3))",
+        },
+        text: {
+          100: "hsl(var(--text-1))"
+        },
+        activity: {
+          "online": "hsl(var(--activity-online))",
+          //idle, do not disturb
+        }
       },
+      borderRadius: {
+        'xs': '2px',
+        'sm': '4px',
+        'md': '8px',
+        'lg': '12px',
+      }
     },
   },
   plugins: [],
