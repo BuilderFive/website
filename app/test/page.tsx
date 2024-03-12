b81edb68aad6690c5230de54ec5b3571cd4c498f
Wrys <wrysplays@gmail.com>
2024-03-12 10:37:40 -0400

    Created Test Page

---
+++ app/test/page.tsx
@@ -0,0 +1,16 @@
+import DeployButton from "../../components/DeployButton";
+import AuthButton from "../../components/AuthButton";
+import { createClient } from "@/utils/supabase/server";
+import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
+import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
+import Header from "@/components/Header";
+import UserProfile from "@/components/sidebar/props";
+
+export default function Test() {
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <UserProfile/>
+    </div>
+  );
+}
