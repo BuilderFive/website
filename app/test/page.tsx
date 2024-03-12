59bf3fc0c5e8c59b7747faef2703cd4ec68f2386
Wrys <wrysplays@gmail.com>
2024-03-12 15:27:29 -0400

    Reorganizing

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,13 +4,15 @@ import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
-import UserProfile from "@/components/sidebar/props";
+import { SideBarProfile } from "@/components/sidebar/sidebars";
 
 export default function Test() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <UserProfile/>
+      <div className="absolute self-end">
+        <SideBarProfile/>
+      </div>
     </div>
   );
 }
--- components/sidebar/create-project-button.tsx
+++ components/sidebar/create-project-button.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { useRef } from "react";
+
 export default function Search() {
     function search(formData: { get: (arg0: string) => any; }) {
       const query = formData.get("query");
@@ -14,15 +16,17 @@ export default function Search() {
   }
 
 export const CreateProject = () => {
+
+    const ref = useRef<HTMLFormElement | null>(null)
+
     function submit(formData: { get: (arg0: string) => any; }) {
         const query = formData.get("query");
         alert(`You added '${query}'`);
+        ref.current?.reset()
         //add project to database and update cache
     }
-
-
-
-    return <form action={submit} className="justify-center whitespace-nowrap">
+    
+    return <form ref={(newRef)=> ref.current = newRef} action={submit} className="justify-center whitespace-nowrap">
         <input type="text" className="focus:outline-none w-full text-xs font-medium  px-5 py-2 rounded-md border-primary-100 border-[2px]" id="create-project" placeholder="Type here to create..." />
      </form>
 }
