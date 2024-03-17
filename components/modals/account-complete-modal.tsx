076130ca90f98cac7ab0d5972453b3c2c89f2587
Wrys <wrysplays@gmail.com>
2024-03-17 14:58:04 -0400

    Added display name setup

--- components/modals/account-complete-modal.tsx
+++ components/modals/account-complete-modal.tsx
@@ -7,6 +7,7 @@ import { toast } from "react-toastify";
 export const UserNotFoundModal = () => {
   const { profile, supabase, user } = useSession();
   const [username, setUsername] = useState("");
+  const [display_name, setDisplayName] = useState("");
 
   //this should be a session context method!!!
   async function createAccount() {
@@ -16,9 +17,10 @@ export const UserNotFoundModal = () => {
       // insert the account into supabase
       const { data, error } = await supabase
         .from("account")
-        .insert([{ username, uuid: user?.id }]).select("*"). single(); 
+        .insert([{ username, display_name, uuid: user?.id }]).select("*"). single(); 
 
       if (error) {
+        toast.error(error.message);
         toast.error("Only lowercase alphanumeric or username already exists");
       } else {
         toast.success("Account created successfully");
@@ -35,7 +37,7 @@ export const UserNotFoundModal = () => {
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
           <div className="p-8 border w-96 shadow-lg rounded-md bg-white">
             <div className="flex flex-col items-center justify-center h-full text-black">
-              <h1 className="text-4xl font-bold">
+              <h1 className="text-2xl font-bold">
                 Finish Setting Up Your Account
               </h1>
               <p className="text-lg">Give us a username</p>
@@ -44,10 +46,15 @@ export const UserNotFoundModal = () => {
                 className="border border-gray-300 rounded-md px-4 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-primary-500"
                 onChange={(e) => setUsername(e.target.value)}
               />
+              <p className="text-lg">Give us a displayed name</p>
+              <input
+                type="text"
+                className="border border-gray-300 rounded-md px-4 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-primary-500"
+                onChange={(e) => setDisplayName(e.target.value)}
+              />
               <button
                 className="bg-primary-100 px-5 py-2 rounded-md mt-5"
-                onClick={() => createAccount()}
-              >
+                onClick={() => createAccount()}>
                 Submit
               </button>
             </div>
