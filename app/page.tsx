9079d22b9bdcf6a80a32ef51bc5b467cf29d6ecf
Wrys <wrysplays@gmail.com>
2024-05-13 17:36:00 -0400

    Changes idk

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,27 @@
 import { useSession } from "@/utils/hooks/SessionContext";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { ReactElement, ReactNode, useEffect, useRef, useState } from "react";
 import { toast } from "react-toastify";
 import SocialButtons from "@/components/SocialButtons";
+import { Router } from "next/router";
+import { FaHome } from "react-icons/fa";
+import { MdEvent } from "react-icons/md";
+import { FaRocket } from "react-icons/fa";
+import { useIsMobile } from "@/utils/hooks/Helper";
+import { GiHamburgerMenu } from "react-icons/gi";
+
 
 export default function Index() {
   return (
-    <div className="size-full relative flex flex-col overflow-x-hidden overflow-y-hidden">
-      {/* Header */}
-      <div style={{ zIndex: 1000 }}>
-        <Header />
-      </div>
-      {/* Background */}
-      <div className="bg-cover" style={{ backgroundImage: 'url("/map.jpg")' }}>
-        {/* Hero */}
+    <div className="size-full relative flex flex-col">
+      
+      <Header />
+
+      <Background>
         <Hero />
-      </div>
+      </Background>
+
       <SocialButtons />
     </div>
   );
@@ -29,8 +34,10 @@ export default function Index() {
  * @returns
  */
 const Header = () => {
-  const { user, logout } = useSession();
   const [isTop, setIsTop] = useState(true);
+  const [menuOpened, setOpenMenu] = useState(false);
+  const router = useRouter();
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -46,51 +53,64 @@ const Header = () => {
 
   return (
     <div
-      className={`w-full fixed top-0 z-10 ${
-        isTop ? "transparent" : "bg-primary-300 shadow-lg"
-      } h-[64px] flex flex-col justify-center`}
-    >
+      className={`w-full fixed top-0 z-10 ${!isTop && "shadow-md bg-background"} h-[64px] flex flex-col justify-center`}>
       <div className="flex flex-row justify-between mx-[24px] items-center">
-        <h1
-          className={`text-3xl font-bold ${
-            isTop ? "text-text-100" : "text-primary-100"
-          }`}
-        >
-          {/* Innov8rs.io */} BuilderFive
+        <h1 className={`text-3xl font-bold ${isTop ? "text-text-100" : "text-primary-100"} ${isMobile && "text-xl"}`}>
+          BuilderFive
         </h1>
 
         {/* Spacer */}
         <div className="flex-grow"></div>
-        {/* Button */}
-        <a href="/newsletter">
-        <button className="text-sm text-primary-100 py-2 px-4 justify-center rounded-md no-underline bg-primary-300 hover:text-primary-200 font-bold">Newsletter</button>
-        </a>
+
+        {isMobile ? (
+          <div className="flex flex-row items-center gap-x-[2rem]">
+            <button onClick={()=>router.push("/events")} type="button" className="text-lg py-[2px] px-[4px] justify-center rounded-md no-underline text-text-100 hover:text-text-200 font-bold items-center"><MdEvent/></button>
+            <button onClick={()=>router.push("/features")} type="button" className="text-lg py-[2px] px-[4px] justify-center rounded-md no-underline text-text-100 hover:text-text-200 font-bold items-center"><FaRocket/></button>
+          </div>
+        ) : (
+          <div className="flex gap-x-[2rem]">
+            <button onClick={()=>router.push("/events")} type="button" className="text-lg py-[2px] px-[4px] justify-center rounded-md no-underline text-text-100 hover:text-text-200 font-bold gap-x-[8px] flex flex-row items-center"><MdEvent/>Events</button>
+            <button onClick={()=>router.push("/features")} type="button" className="text-lg py-[2px] px-[4px] justify-center rounded-md no-underline text-text-100 hover:text-text-200 font-bold gap-x-[8px] flex flex-row items-center"><FaRocket/>Features</button>
+          </div>
+        )}
+        
       </div>
     </div>
   );
 };
 
+/**
+ * Opaque picture of a group of people networking and animated background
+ */
+const Background = ({ children }: any) => {
+  return (
+    <div className="bg-cover bg-no-repeat bg-[url('/wave.svg')]">
+        {children}        
+    </div>
+  );
+}
+
 const Hero = () => {
   const { supabase, rsvpCount, insertRSVP } = useSession();
   const [email, setEmail] = useState("");
+  const isMobile = useIsMobile();
 
   return (
-    <div className="bg-black bg-opacity-70 min-h-screen flex justify-center items-center">
-      <div className="text-center text-white flex flex-col items-center mt-[4rem]">
-        <h1 className="text-[3rem] font-bold mb-4">
-          Share Projects <br />
-          Meet Innovators <br />
-          Join the Community
+    <div className="min-w-screen min-h-screen flex justify-center items-center mt-[4rem]">
+      <div className="text-center flex flex-col items-center px-[2rem]">
+        <h1 className={`text-[3rem] font-bold mb-4 ${isMobile && "text-[30px]"}`}>
+          Connect with local <br />
+          Entrereneurs on campus <br />
         </h1>
-        <p className="text-[2rem] max-w-[480px] mb-8">
-          Meet with local like-minded innovators on your campus in person
+        <p className={`text-[2rem] max-w-[560px] mb-8 ${isMobile && "text-[24px]"}`}>
+          Attend weekly networking events and mark your project's updates by location on your campus map
         </p>
-        {/* <div className="mb-4 flex flex-col gap-[8px] mx-[24px]">
+        <div className="mb-4 flex flex-col gap-[8px] mx-[24px]">
           <div className="p-[12px] rounded-md bg-white">
             <p className="self-start flex text-lg font-black text-primary-300">
-              * {count == 0 ? "loading" : count}/200 spots left
+              {rsvpCount}/200 spots left
             </p>
-            <form action={handleInsert}>
+            <form action={()=>insertRSVP(email)}>
               <input
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -102,25 +122,13 @@ const Hero = () => {
               />
               <button
                 type="submit"
-                className="bg-primary-300 hover:text-primary-200 rounded-e-md text-white font-bold py-2 px-5 shadow-lg"
+                className="bg-primary-300 hover:text-primary-200 rounded-e-md text-text-1 font-bold py-2 px-5 shadow-lg"
               >
                 SIGN UP
               </button>
             </form>
-          </div> */}
-
-        {/* <div className="mt-[12px]">
-            <p className="text-bold">Got invited?</p>
-            <input
-              type="email"
-              className="py-2 px-3 shadow-lg text-black w-64 rounded-s-md focus:outline-none focus:ring-2 focus:ring-primary-500"
-              placeholder="Enter your access code"
-            />
-            <button className="bg-primary-200 hover:text-primary-100 rounded-e-md text-white font-bold py-2 px-5 shadow-lg">
-              SUBMIT
-            </button>
-          </div> */}
-        {/* </div> */}
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -146,11 +154,8 @@ const RevealOnScroll = ({ children }: { children: ReactNode }) => {
     };
   }, []);
 
-  const classes = `transition-opacity duration-1000
-      ${isVisible ? "opacity-100" : "opacity-0"}`;
-
   return (
-    <div ref={ref} className={classes}>
+    <div ref={ref}>
       {children}
     </div>
   );
