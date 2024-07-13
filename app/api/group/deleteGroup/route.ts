79477fb6b754cb7be6b3462e621853d61ea8e847
Wrys <wrysplays@gmail.com>
2024-07-12 22:35:19 -0500

    test

--- app/api/group/deleteGroup/route.ts
+++ app/api/group/deleteGroup/route.ts
@@ -1,11 +1,47 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 import { supabase } from '~/util/supabaseClient';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+    const test = {
+        "event": "room_finished",
+        "room": {
+          "sid": "RM_hycBMAjmt6Ub",
+          "name": "Demo Room",
+          "emptyTimeout": 300,
+          "creationTime": "1692627281",
+          "turnPassword": "2Pvdj+/WV1xV4EkB8klJ9xkXDWY=",
+          "enabledCodecs": [
+            {
+              "mime": "audio/opus"
+            },
+            {
+              "mime": "video/H264"
+            },
+            {
+              "mime": "video/VP8"
+            },
+            {
+              "mime": "video/AV1"
+            },
+            {
+              "mime": "video/H264"
+            },
+            {
+              "mime": "audio/red"
+            },
+            {
+              "mime": "video/VP9"
+            }
+          ]
+        },
+        "id": "EV_3DXLrqmZCLEF",
+      }
+    
     try {
-        const { randomInfo } = await req.json();
-
+        const { randomInfo } = await req.body()
+        
         const group_uuid = randomInfo.name;
 
         if (group_uuid == null) return NextResponse.json({ error: 'A group identifier is required' }, { status: 500 });
@@ -13,9 +49,9 @@ export async function POST(req: NextRequest, res: NextResponse) {
         const { data, error } = await supabase.from('groups').delete().eq('group_uuid', group_uuid);
 
         if (error) {
-            return NextResponse.json({ error: error.message }, { status: 500 });
+            return NextResponse.json({ error: error.message, randomInfo }, { status: 500 });
         }
-        return NextResponse.json({ data }, { status: 200 });
+        return NextResponse.json({ data, randomInfo }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 });
     }
