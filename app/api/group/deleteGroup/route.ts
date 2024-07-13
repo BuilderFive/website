c966c0e4370f1b79c5ebc6e3922495d38e360813
Wrys <wrysplays@gmail.com>
2024-07-12 22:05:57 -0500

    trying something out

--- app/api/group/deleteGroup/route.ts
+++ app/api/group/deleteGroup/route.ts
@@ -6,14 +6,14 @@ export async function POST(req: NextRequest, res: NextResponse) {
     try {
         const { randomInfo } = await req.json();
 
-        const group_uuid = randomInfo.id;
+        const group_uuid = randomInfo.name;
 
         if (group_uuid == null) return NextResponse.json({ error: 'A group identifier is required' }, { status: 400 });
 
         const { data, error } = await supabase.from('groups').delete().eq('group_uuid', group_uuid).select().single();
 
         if (error) {
-            return NextResponse.json({ error: error.message }, { status: 500 });
+            //return NextResponse.json({ error: error.message }, { status: 500 });
         }
         return NextResponse.json({ data }, { status: 200 });
     } catch (error) {
