717ad92e4d258ad02b3cda66b6acb64e5b2f1ae1
Wrys <wrysplays@gmail.com>
2024-07-21 13:44:11 -0500

    Rerouted to connect page

--- app/(landing)/(home)/components/CtaButton.tsx
+++ app/(landing)/(home)/components/CtaButton.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { useEffect, useRef, useState } from 'react';
+import { FaSpinner } from 'react-icons/fa';
+import { FaRegHandPointer } from 'react-icons/fa6';
 import { useSession } from '~/util/AuthProvider';
 
 interface CtaButtonProps {
@@ -9,14 +12,18 @@ interface CtaButtonProps {
 
 export const CtaButton: React.FC<CtaButtonProps> = ({ children }) => {
     const ref = useRef<HTMLButtonElement>(null);
-    const { handleSignInWithGoogle } = useSession();
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
 
     return (
-<button onClick={handleSignInWithGoogle}
+<button onClick={()=> {
+    router.push('/connect')
+    setLoading(true)
+}}
         ref={ref}
         className="relative w-full flex flex-row rounded-[24px] p-[6px] items-center justify-center bg-gradient-to-r from-[var(--secondary-4)] to-[var(--secondary-1)] hover:cursor-pointer bg-[length:200%_200%] animate-gradient-x">
     <div className='w-full h-full flex flex-row gap-[12px] rounded-[24px] p-[24px] justify-center items-center bg-white'>
-        <img className='w-6 h-6' src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google logo"/>
+        {loading ? <FaSpinner className="animate-spin text-secondary1" size='24px'/> : <FaRegHandPointer size='24px' className='text-secondary1'/>}
         <p className='text-secondary1 font-semibold text-2xl max-md:text-lg'>{children}</p>
     </div>
 </button>
